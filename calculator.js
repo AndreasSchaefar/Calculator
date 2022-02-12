7c28f147ec6eb483ae6790e5a1dfebea88c0f5fe
AndreasSchaefar <skachko.andrii@gmail.com>
2022-02-12 22:22:15 +0200

    Wrote tests

--- calculator.js
+++ calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const { reduce } = require("lodash");
+
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.length ? reduce(a, (prev, curr) => prev + curr) : 0;
 };
 
-const multiply = function() {
-
+const multiply = function(a,b) {
+    return reduce(a, (prev, curr) => prev * curr);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+    return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0 || a == 1) {
+        return 1;
+    } else if (a < 0) {
+        return -1;
+    };
+    return a * factorial(a-1);
 };
 
 // Do not edit below this line
