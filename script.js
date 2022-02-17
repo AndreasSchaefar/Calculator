346612b62a6cbf2930538cdbc01f57bc90b1f129
AndreasSchaefar <skachko.andrii@gmail.com>
2022-02-17 10:42:35 +0200

    created populate function

--- script.js
+++ script.js
@@ -1,65 +1,33 @@
-class Calculator { 
+class Calculator {
     constructor(previousOperationTextElement, currentOperationTextElement)  {
     this.previous = previousOperationTextElement;
     this.current = currentOperationTextElement;
-    this.result = 0;
-    this.equation = "";
-    this.previousOperand = Number(this.previous.textContent);
-    this.currentOperand = Number(this.current.textContent);
     }
 
     clear() {
         this.previous.textContent = "";
-        this.current.textContent = "0";
+        this.current.textContent = 0;
     }
 
-    enterDigit(number) {
-        if (number === "0") {
+    // if current is not 0 and num is not period append
+    // if current is 0 and num is not period set content to num
+    // if current is not 0 and doesn't have period append num
+    // 
+    populate(num) {
+        let value = this.current.textContent;
+        if (value.includes(".") & num === ".") {
             return;
+        } else if (num !== "." & value === "0") {
+            this.current.textContent = num
         } else {
-            this.current.textContent += number;
+            this.current.textContent += num
         }
-
     }
-
-    add() {
-        this.result = this.previousOperand + this.currentOperand;
-        this.equation = `${this.previousOperand} + ${this.currentOperand} =`;
-    }
-
-    subtract() {
-        this.result = this.previousOperand - this.currentOperand;
-        this.equation = `${this.previousOperand} - ${this.currentOperand} =`;
-    }
-
-    multiply() {
-        this.result = this.previousOperand * this.currentOperand;
-        this.equation = `${this.previousOperand} × ${this.currentOperand} =`;
-    }
-
-    divide() {
-        this.result = this.previousOperand / this.currentOperand;
-        this.equation = `${this.previousOperand} ÷ ${this.currentOperand} =`;
-    }
-
-    remainder() {
-        this.result = this.previousOperand % this.currentOperand;
-        this.equation = `${this.previousOperand} % ${this.currentOperand} =`;
-    }
-
-    setSign() {
-        this.currentOperand = -this.currentOperand;
-        this.current.textContent = this.currentOperand;
-    }
-
-    equals() {
-        this.currentOperand = this.result;
-        this.current.textContent = this.result;
-        this.previous.textContent = this.equation;
-        this.previousOperand = this.result; 
-    }
-    
 };
+
+function callClear() {
+    calculator.clear();
+}
 
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
@@ -70,26 +38,9 @@ const currentOperationTextElement = document.querySelector("[data-current-operat
 
 const calculator = new Calculator(previousOperationTextElement, currentOperationTextElement);
 
-const operationsMap = {
-    "±": () => {calculator.setSign()},
-    "%": () => {calculator.remainder()},
-    "÷": () => {calculator.divide()},
-    "×": () => {calculator.multiply()},
-    "-": () => {calculator.subtract()},
-    "+": () => {calculator.add()}
-}
-
-
-
-Array.from(operationButtons).forEach(button => {
-    button.addEventListener("click", operationsMap[button.textContent]);
-})
-
+clearButton.addEventListener("click", callClear);
 Array.from(numberButtons).forEach(button => {
-    button.addEventListener("click", (e) => {
-        calculator.enterDigit(e.target.textContent);
+    button.addEventListener("click", e => {
+        calculator.populate(e.target.textContent);
     })
 })
-
-equalsButton.addEventListener("click", () => {calculator.equals()});
-clearButton.addEventListener("click", () => {calculator.clear()});
